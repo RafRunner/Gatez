f3f6cf3748ad5e3fa614ac011189738d6da7e748
Rafael Santana <rafaelns.br@gmail.com>
2022-09-04 00:56:02 -0300

    Adding temporary hack to make Flip-Flops easier to build

--- src/js/game/systems/edge_detector.js
+++ src/js/game/systems/edge_detector.js
@@ -3,13 +3,15 @@
  * Thanks to SkimnerPhi for providing the original code and art associated with this file
  */
 
- import { EdgeDetectorComponent, enumEdgeDetectorType } from "../components/edge_detector";
- import { GameSystemWithFilter } from "../game_system_with_filter";
- import { castBool, isTruthyItem } from "../items/boolean_item";
+import { EdgeDetectorComponent, enumEdgeDetectorType } from "../components/edge_detector";
+import { GameSystemWithFilter } from "../game_system_with_filter";
+import { castBool, isTruthyItem } from "../items/boolean_item";
 
 export class EdgeDetectorSystem extends GameSystemWithFilter {
     constructor(root) {
         super(root, [EdgeDetectorComponent]);
+        this.outputFrames = 2;
+        this.framesCount = 0;
     }
 
     update() {
@@ -29,6 +31,13 @@ export class EdgeDetectorSystem extends GameSystemWithFilter {
                 continue;
             }
             const inputValue = inputNetwork ? inputNetwork.currentValue : null;
+
+            // TODO Hack to make Flip-Flops easier to build. Improve in the future
+            this.framesCount++;
+            if (this.framesCount < this.outputFrames) {
+                return;
+            }
+            this.framesCount = 0;
 
             let output = false;
             switch (edgeComp.type) {
